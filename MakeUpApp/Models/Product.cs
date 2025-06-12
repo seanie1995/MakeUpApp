3f80812c8c1bada4b0d1bc6e4d3489dac19c656e
Sean Schelin <119659530+seanie1995@users.noreply.github.com>
2025-06-12 11:52:36 +0200

    Added some preliminary models

---
+++ MakeUpApp/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace MakeUpApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime OpenDate { get; set; }
+        public int PAO { get; set; }
+        public int UserId { get; set; }
+        public User? User { get; set; }
+        public ProductType? ProductType { get; set; }
+    }
+}
---
+++ MakeUpApp/Models/ProductType.cs
@@ -0,0 +1,10 @@
+﻿namespace MakeUpApp.Models
+{
+    public class ProductType
+    {
+       public int id {  get; set; }
+       public string name { get; set; }
+       public ICollection<Product>? products { get; set; } 
+
+    }
+}
